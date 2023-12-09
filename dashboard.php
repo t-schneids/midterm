aea1353991d4fb89da4ac58ff0e753a688b3c6e2
lindsaygarf <lindsay.garfinkel@tufts.edu>
2023-12-09 16:09:48 -0500

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,40 +1,44 @@
 <?php 
-session_start();
+    session_start();
 
-if (!isset($_SESSION['user']) || !isset($_SESSION['logged']) || !$_SESSION['logged']) {
-    header("Location: login.php");
-    exit();
-}
+    if (!isset($_SESSION['user']) || !isset($_SESSION['logged']) || !$_SESSION['logged']) {
+        header("Location: login.php");
+        exit();
+    }
 
-// Retrieve user's email from the session
-$email = $_SESSION['user'];
+    // Retrieve user's email from the session
+    $email = $_SESSION['user'];
 
 
-// Establish connection info
-$server = "localhost"; 
-$userid = "urre4ivsfgzys"; 
-$pw = "DogDays12!"; 
-$db = "db5nvjnj3daedb"; 
+    // Establish connection info
+    $server = "35.212.69.145"; 
+    $userid = "urre4ivsfgzys"; 
+    $pw = "DogDays12!"; 
+    $db = "db5nvjnj3daedb"; 
 
-// Create connection
-$conn = new mysqli($server, $userid, $pw, $db);
+    // Create connection
+    $conn = new mysqli($server, $userid, $pw, $db);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-// Prepare and execute SQL query
-$query = "SELECT * FROM users WHERE email='$email'";
-$result = $conn->query($query);
-$userID = $_SESSION['userID'];
+    // Prepare and execute SQL query
+    $query = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($query);
+    $userID = $_SESSION['userID'];
 
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $firstName = $row['firstName']; 
-    $lastName = $row['lastName'];
-    // Display the dashboard with the user's information
-}
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $firstName = $row['firstName']; 
+        $lastName = $row['lastName'];
+        // Display the dashboard with the user's information
+    }
+
+    // SQL query for donation information
+    $query2 = "SELECT Item, quantity, Price FROM donations WHERE userID='$userID'";
+    $result2 = $conn->query($query2);
 ?>
 
 <html>
@@ -51,73 +55,73 @@ if ($result->num_rows > 0) {
             footer {
                 position: relative !important;
             }
+
             h1 {
                 text-align: center;
                 color: rgb(209,21,28);
             }
 
-            label {
-                width: 130px;
+            .dash {
+                align-items: center;
+                text-align: center;
                 display: flex;
+                justify-content: center;
             }
 
-            form {
-                background-color: rgba(152, 106, 79, 0.2);
-                padding: 10px;
+            td {
+                padding: 10px 40px;
+            }
+
+            .info {
+                height: 500px;
                 border-radius: 25px;
+                background-color: rgba(152, 106, 79, 0.2);
+                width: 360px;
+                max-width: 360px;
             }
 
-            .contact a {
+            .title {
+                font-size: 35px;
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                text-align: center;
+            }
+
+            .logout a {
                 text-decoration: none;
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
                 font-weight: 800;
                 color: black;
             }
 
-            .contact {
+            .logout {
                 width: fit-content;
-                background-color: rgb(246, 207, 66);
-                border-radius: 25px;
+                background-color: rgba(209,21,28, 0.6);
                 padding: 14px 16px;
             }
 
-            .row {
+            .button {
+                justify-content: center;
                 display: flex;
-                flex-direction: row;
-                flex-wrap: wrap;
-                padding-left: 10px;
-                padding-right: 5px;
-                padding-bottom: 30px;
-                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                margin-bottom: 15px;
+                margin-top: 7px;
             }
 
-            .column {
-                display: flex;
-                flex-direction: column;
-                flex: 50%;
-                align-items: center;
-            }
-            .search-container {
-                display: flex;
-                align-items: center;
-                margin-bottom: 10px;
-            }
+            @media (max-width: 1200px) {     
+                td {
+                    text-align: center;
+                    margin-bottom: 20px; 
+                    padding: 5px;
+                }
 
-            label {
-                white-space: nowrap;
-            }
-
-            #breedInput {
-                margin-left: 5px; 
-            }
-
-            label, input, button {
-                margin-right: 10px; 
-            }
-            .dog-input {
-                flex-grow: 1;
+                tr {
+                    display: flex;
+                    flex-wrap: wrap;
+                    width: fit-content;
+                    align-items: center;
+                    justify-content: center;
+                }
             }
         </style>
- 
 
     </head>
     <body>
@@ -126,15 +130,17 @@ if ($result->num_rows > 0) {
             <div class="logo">
                 <li> 
                     <a href="index.html"> 
-                        <img src="logo.png" alt="company logo">
+                        <img src="wagonLogo.png" alt="company logo">
                     </a> 
                 </li>
+                <li class="newTabs" id="companyName"> Rescue Waggin</li>
             </div> 
             </ul>
         </nav>
+
         <ul class="profile">
             <li>
-                <a class="newTabs" href="dashboard.php">Dasboard</a>
+                <a class="newTabs" href="dashboard.php">Dashboard</a>
             </li>
         </ul>
         <ul class="cart">
@@ -142,7 +148,6 @@ if ($result->num_rows > 0) {
                 <a class="newTabs" href="shoppingCart.php"><img src="images/cart.png" width="20"></a>
             </li>
         </ul>
-
         <ul class="tabGroup">
             <li> 
                 <a class="tabs" href="aboutUs.html" > OUR STORY</a> 
@@ -151,10 +156,10 @@ if ($result->num_rows > 0) {
                 <a class="tabs" href="rescues.html"> RECENT RESCUES</a>
             </li>
             <li>
-            <a class="tabs" href="adoption.php" id="current"> ADOPTION</a>
+            <a class="tabs" href="adoption.php"> ADOPT A DOG</a>
             </li>
             <li>
-                <a class="tabs" href="availableDogs.php"> AVAILABLE DOGS</a>
+                <a class="tabs" href="availableDogs.php" id="current"> AVAILABLE DOGS</a>
             </li>
             <li>
                 <a class="tabs" href="contact.html"> CONTACT US</a>
@@ -172,26 +177,44 @@ if ($result->num_rows > 0) {
             <div class="closeIcon material-icons"> close</div>
         </button>
 
+        <div>
+            <h1>Welcome to your dashboard, <?php echo htmlspecialchars($firstName . ' ' . $lastName); ?></h1>
+        </div>
 
-            <div>
-                <h1>Dashboard</h1>
-                <h2>Welcome, <?php echo htmlspecialchars($userID . '  ' .  $firstName . ' ' . $lastName); ?></h2>
+        <table class="dash">
+            <tr>
+                <td> 
+                    <div class="title"> Recent Donations </div>
+                    <div id = "donation" class="info"> </div>
+                </td>
+                <td> 
+                    <div class="title"> Recent Purchases </div>
+                    <div class="info"> </div>
+                </td>
+                <td> 
+                    <div class="title"> Adoption Status </div>
+                    <div class="info"> </div>
+                </td>
+            </tr>
+        </table>
+
+        <div class="button">
+            <div class="logout">
+                <a href="logout.php">Log Out</a>
             </div>
+        </div>
 
-            <footer>
-                <h4> &copy; 2017 Rescue Waggin' </h4>
-                <ul class="nav">
-                    <li> <a href="https://www.gmail.com"><img src="images/gmailLogo.png" style="width:25px;height:20px;"></a> </li>
-                    <li> <a href="https://www.instagram.com"><img src="images/instagramLogo.png" style="width:20px;height:20px;"></a> </li>
-                    <li> <a href="https://www.facebook.com"><img src="images/facebookLogo.png" style="width:20px;height:20px;"></a> </li>
-                </ul>
-            </footer>
+        <footer>
+            <h4> &copy; 2017 Rescue Waggin' </h4>
+            <ul class="nav">
+                <li> <a href="https://www.gmail.com"><img src="images/gmailLogo.png" style="width:25px;height:20px;"></a> </li>
+                <li> <a href="https://www.instagram.com"><img src="images/instagramLogo.png" style="width:20px;height:20px;"></a> </li>
+                <li> <a href="https://www.facebook.com"><img src="images/facebookLogo.png" style="width:20px;height:20px;"></a> </li>
+            </ul>
+        </footer>
         </div>
     </body>
 </html>
-
-
-
 
 
 <?php
